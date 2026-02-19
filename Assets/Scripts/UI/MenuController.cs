2c3905a5112ff3b31406e983154d01c761f20139
Dawid Zaguła <dawid.zagula23@gmail.com>
2026-02-19 15:28:27 +0100

    feat: more tweaking on main menu

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
+using Zenject;
 
 public class MenuController : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class MenuController : MonoBehaviour
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _optionMenu;
     [SerializeField] private GameObject _creditsMenu;
-    [SerializeField] private GameObject _exitConfirmationMenu;
+    [SerializeField] private GameObject _quitGameConfirmationMenu;
     [SerializeField] private GameObject _backToMenuConfirmationMenu;
 
     [Header("Buttons - Show/Hide per scene")]
@@ -33,6 +34,14 @@ public class MenuController : MonoBehaviour
     private bool _isPaused = false;
     private bool _isMainMenu;
 
+    private GameStateManager _gameStateManager;
+
+    [Inject]
+    public void Construct(GameStateManager gameStateManager)
+    {
+        _gameStateManager = gameStateManager;
+    }
+
     void Start()
     {
         _isMainMenu = SceneManager.GetActiveScene().name == "GameMenuScene";
@@ -41,6 +50,7 @@ public class MenuController : MonoBehaviour
         {
             _mainMenu.SetActive(true);
             _startButton.SetActive(true);
+            _creditsButton.SetActive(true);
             //_exitButton.SetActive(true); // for desktop release only
             _backToMenuButton.SetActive(false);
 
@@ -51,6 +61,7 @@ public class MenuController : MonoBehaviour
             _backToMenuButton.SetActive(true);
             _mainMenu.SetActive(false);
             _startButton.SetActive(false);
+            _creditsButton.SetActive(false);
             _exitButton.SetActive(false);
             if (_resumeButton != null) _resumeButton.SetActive(true);
         }
@@ -61,12 +72,32 @@ public class MenuController : MonoBehaviour
 
     void Update()
     {
-        if (!_isMainMenu && Keyboard.current.escapeKey.wasPressedThisFrame)
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
             if (_optionMenu.activeSelf)
+            {
                 OnBackToMenu();
-            else
+                return;
+            }
+            else if (_creditsMenu.activeSelf)
+            {
+                OnBackToMenuFromCredits();
+                return;
+            }
+            else if (_backToMenuConfirmationMenu.activeSelf)
+            {
+                OnBackToPauseFromMenuExitConfirmation();
+                return;
+            }
+            else if (_quitGameConfirmationMenu.activeSelf)
+            {
+                //OnBackToMenuFromQuitGameConfirmation();
+                //return;
+            }
+            else if (!_isMainMenu)
+            {              
                 TogglePause();
+            }
         }
     }
 
@@ -74,8 +105,19 @@ public class MenuController : MonoBehaviour
     public void TogglePause()
     {
         _isPaused = !_isPaused;
-        _mainMenu.SetActive(_isPaused);
+
+        if (_isPaused)
+        {
+            _gameStateManager.ChangeCurrentState(GameState.Paused);
+        }
+        else
+        {
+            _gameStateManager.ChangeCurrentState(GameState.Started);
+        }
+
+            _mainMenu.SetActive(_isPaused);
         Time.timeScale = _isPaused ? 0f : 1f;
+
     }
 
     public void OnResumeButton()
@@ -92,12 +134,25 @@ public class MenuController : MonoBehaviour
         SceneLoader.ProcessLoadScene(GameScene.Level_1);
     }
 
-    public void OnExitButton()
+    public void OnOpenQuitConfirmButton()
     {
-        Application.Quit();
+        _mainMenu.SetActive(false);
+        _quitGameConfirmationMenu.SetActive(true);
     }
 
-    public void OnBackToMainMenu()
+    //For desktop release only
+    //public void OnConfirmExitButton()
+    //{
+    //    Application.Quit();
+    //}
+
+    public void OnOpenBackToMainMenuConfirmation()
+    {
+        _mainMenu.SetActive(false);
+        _backToMenuConfirmationMenu.SetActive(true);
+    }
+
+    public void OnConfirmBackToMainMenu()
     {
         Time.timeScale = 1f;
         SceneLoader.ProcessLoadScene(GameScene.MainMenu);
@@ -114,21 +169,40 @@ public class MenuController : MonoBehaviour
     public void OnCreditsButton()
     {
         _mainMenu.SetActive(false);
-
+        _creditsMenu.SetActive(true);
     }
 
     public void OnBackToMenu()
     {
-        if (_optionMenu.activeInHierarchy)
-        {
-            _optionMenu.SetActive(false);
-        }
+        _optionMenu.SetActive(false);
 
         _mainMenu.SetActive(true);
         SaveSettings();
     }
 
-   // ---- AUDIO ----
+    public void OnBackToMenuFromCredits()
+    {
+        _creditsMenu.SetActive(false);
+
+        _mainMenu.SetActive(true);
+    }
+
+    //For desktop release only
+    //public void OnBackToMenuFromQuitGameConfirmation()
+    //{
+    //    _quitGameConfirmationMenu.SetActive(false);
+
+    //    _mainMenu.SetActive(true);
+    //}
+
+    public void OnBackToPauseFromMenuExitConfirmation()
+    {
+        _backToMenuConfirmationMenu.SetActive(false);
+
+        _mainMenu.SetActive(true);
+    }
+
+    // ---- AUDIO ----
     public void OnMusicVolumeChanged(float value)
     {
         if (MusicPlayer.Instance != null)
